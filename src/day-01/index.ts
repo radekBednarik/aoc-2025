fb0b6512fb2b2a70545d7b2113d626554fca6a0a
radekBednarik <radek.bednarik@tesena.com>
2025-12-01 08:06:27 +0100

    feat: Day 1 part one done!

--- src/day-01/index.ts
+++ src/day-01/index.ts
@@ -2,8 +2,27 @@ import { readInput } from "../common/utils.js";
 
 async function main() {
 	const rawData = await readInput("src/day-01/input.txt");
+	const instructions = rawData.trim().split("\n");
 
-	console.log(rawData);
+	let currentPosition = 50;
+	let counter = 0;
+
+	for (const instruction of instructions) {
+		const direction = instruction.slice(0, 1);
+		const amount = Number.parseInt(instruction.slice(1));
+
+		if (direction === "L") {
+			currentPosition = currentPosition - amount;
+		} else {
+			currentPosition = currentPosition + amount;
+		}
+
+		if (currentPosition % 100 === 0) {
+			counter = counter + 1;
+		}
+	}
+
+	console.log(counter);
 }
 
 await main();
